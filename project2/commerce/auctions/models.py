61d9db50f2627f6b12d337cd09ad1baf4be3224f
brianle66 <leminhnghia6695@gmail.com>
2024-02-23 23:23:00 +0700

    project2 update migrated models

--- project2/commerce/auctions/models.py
+++ project2/commerce/auctions/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Team(models.Model):
+    teamName = models.CharField(max_length = 64)
+    teamCode = models.CharField(max_length = 3)
+    def __str__(self) -> str:
+        return f'Team {self.teamName} - {self.teamCode}'
+    
+class Listing(models.Model):
+    title = models.CharField(max_length = 64)
+    owner = models.ForeignKey(User, on_delete = models.CASCADE,blank = True, null = True, related_name = 'cars')
+    price = models.FloatField()
+    team = models.ForeignKey(Team, on_delete = models.CASCADE,blank = True, null = True, related_name = 'cars')
+    imgURL = models.CharField(max_length = 1000)
+    isActive = models.BooleanField(default = True)
+
+    def __str__(self) -> str:
+        return f'Name: {self.title}'
+
