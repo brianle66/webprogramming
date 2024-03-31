2669620f295b35086f19d5e56ff2406c3dc5f9fd
brianle66 <leminhnghia6695@gmail.com>
2024-03-31 15:57:24 +0700

    finish project3

--- project3/mail/mail/static/mail/inbox.js
+++ project3/mail/mail/static/mail/inbox.js
@@ -14,19 +14,27 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // Compose tab view
-function compose_email() {
-
+function compose_email(email=null) {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
   document.querySelector('#email-content').style.display = 'none';
 
   // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+  if (email) {
+    // If it's a reply, pre-fill the composition fields with the reply data
+    document.querySelector('#compose-recipients').value = email.sender;
+    document.querySelector('#compose-subject').value = `Re: ${email.subject}`;
+    document.querySelector('#compose-body').value = `<type reply here>
+    
+    On ${email.timestamp}, ${email.sender} wrote:\n${email.body}\n\n`;
+  } else {
+    // If it's a new email, clear all fields
+    document.querySelector('#compose-recipients').value = '';
+    document.querySelector('#compose-subject').value = '';
+    document.querySelector('#compose-body').value = '';
+  }
 }
-
 // Mailbox tab view
 function load_mailbox(mailbox) {
   
@@ -57,19 +65,18 @@ function load_mailbox(mailbox) {
             <h6>Time: ${email.timestamp}</h6>
         `
         };
-        element.className = '';
+        element.className = 'list-group-item';
+        // Add class to read email to change BG color
+        if (email.read) {
+          element.classList.add('read')
+        } else {
+          element.classList.add('unread')
+        }
         element.addEventListener('click', function() {
-            // Add class to read email to change BG color
-            if (email.read) {
-              element.classList.add('read')
-            } else {
-              element.classList.add('unread')
-            }
-            console.log('This element has been clicked!')
-            
             // Show email content
             view_email(email.id);
             console.log(element.className)
+            console.log('This element has been clicked!')
         });
         
         document.querySelector('#emails-view').append(element);
@@ -81,23 +88,73 @@ function load_mailbox(mailbox) {
       // ... do something else with emails ...     
   });
 }
+
 function view_email(id){
-  fetch(`/emails/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-        read : true
-    })
-  })
+  fetch(`/emails/${id}`)
   .then(response => response.json())
   .then(email => {
-    // Print email
-    console.log(email);
+      // Print email
+      console.log(email)
+    
+    // Update UI to display email content
+    document.querySelector('#emails-view').style.display = 'block';
+    document.querySelector('#compose-view').style.display = 'none';
     document.querySelector('#email-content').style.display = 'block';
+    document.querySelector('#email-content').innerHTML = `
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item"><h5 class="mb-1">Subject: ${email.subject}</h5></li>
+        <li class="list-group-item"><h6 class="mb-1">From: ${email.sender}</h6></li>
+        <li class="list-group-item"><h6 class="mb-1">To: ${email.recipients.join(', ')}</h6></li>
+        <li class="list-group-item"><p class="mb-1">Time: ${email.timestamp}</p></li>
+        <li class="list-group-item"><p class="mb-1">${email.body}</p></li>
+      </ul>
+    `;
+    if(!email.read){
+      fetch(`/emails/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            read : true
+        })
+      })
+    }
+    //archive btn
+    const archive_btn = document.createElement('button');
+    archive_btn.className = 'btn';
+    archive_btn.innerHTML = email.archived ? 'Unarchive' : 'Archive';
+    archive_btn.classList.toggle('btn-primary', !email.archived);
+    archive_btn.classList.toggle('btn-warning', email.archived);
+    document.querySelector('#email-content').append(archive_btn);
+    archive_btn.addEventListener('click', () => archive_email(email.id));
+    //reply btn
+    const reply_btn = document.createElement('button');
+    reply_btn.className = 'btn btn-success';
+    reply_btn.innerHTML = 'Reply';
+    document.querySelector('#email-content').append(reply_btn);
+    reply_btn.addEventListener('click', () => compose_email(email));
   })
-  
 }
 
-function sent_email(event) {
+function archive_email(id){
+  fetch(`/emails/${id}`)
+  .then (response => response.json())
+  .then (email => {
+    const newArchivedValue = !email.archived;
+    return fetch(`/emails/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            archived: newArchivedValue
+        })
+    });
+  })
+  .then(() => {
+    console.log('Email is archived/unarchived');
+    // Reload the current page
+    window.location.reload();
+  })
+}
+  
+
+function sent_email(event,email) {
   event.preventDefault();
   // Get data from Compose form
   const recipients = document.querySelector('#compose-recipients').value;
