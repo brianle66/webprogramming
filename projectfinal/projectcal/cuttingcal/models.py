9ce0ec5f8e5a045bff22b90c8a2ff2c8b3f5a07a
brianle66 <leminhnghia6695@gmail.com>
2024-05-18 00:07:06 +0700

    working on DB Schema Final Project

--- projectfinal/projectcal/cuttingcal/models.py
+++ projectfinal/projectcal/cuttingcal/models.py
@@ -3,3 +3,40 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self) -> str:
+        return f'Customer name: {self.name}'
+
+class Factory(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='factories')
+    def __str__(self) -> str:
+        return f'Customer: {self.customer} - Factory: {self.name} - Site: {self.location}'
+
+class Project(models.Model):
+    code = models.CharField(max_length=10,primary_key=True)
+    name = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='projects')
+    def __str__(self) -> str:
+        return f'Project: {self.code} Customer: {self.customer}'
+
+class Fabric(models.Model):
+    type = models.CharField(max_length=20)
+    weight = models.IntegerField()
+    ply_height = models.IntegerField()
+
+class Style(models.Model):
+    name = models.CharField(max_length=50)
+    fabric_A = models.ForeignKey(Fabric, on_delete=models.CASCADE, related_name='Astyles')
+    fabric_B = models.ForeignKey(Fabric, on_delete=models.CASCADE, related_name='Bstyles')
+    fabric_C = models.ForeignKey(Fabric, on_delete=models.CASCADE, related_name='Cstyles')
+
+class Order(models.Model):
+    size_set = models.IntegerField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='orders')
+    style = models.ForeignKey(Style, on_delete=models.CASCADE, related_name='orders')
+    
--- projectfinal/projectcal/cuttingcal/views.py
+++ projectfinal/projectcal/cuttingcal/views.py
@@ -9,7 +9,35 @@ from django.views.decorators.csrf import csrf_exempt
 
 from .models import User
 
+
 # Create your views here.
 def index(request):
+    if request.user is authenticate:
+        return render(request, 'cuttingcal/index.html')
+    else: return HttpResponseRedirect('register')
 
-    return render(request, 'cuttingcal/index.html')
+def register(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "cuttingcal/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(email, email, password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "cuttingcal/register.html", {
+                "message": "Email address already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "cuttingcal/register.html")
