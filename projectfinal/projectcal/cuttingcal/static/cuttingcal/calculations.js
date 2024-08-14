1ac234cf63e8674d8eafac3fbf314055381d7b94
brianle66 <leminhnghia6695@gmail.com>
2024-08-14 23:04:23 +0700

    UX fix

--- projectfinal/projectcal/cuttingcal/static/cuttingcal/calculations.js
+++ projectfinal/projectcal/cuttingcal/static/cuttingcal/calculations.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+    document.querySelector('#selectedStyle').addEventListener('submit', (event) => calculateproject(event))
 });
+
+function calculateproject(event){
+    
+}
